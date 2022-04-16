e0d711077444916a303c2b2a91399ffe588fff24
YuhengHe <50586123+YuhengHe@users.noreply.github.com>
2022-04-16 15:48:06 -0700

    create pseudo data for the search

---
+++ src/app.js
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
+import Search from './search';
+import Announcer from './announcer';
+
+const fields = [
+    { id: '1', name: 'Medical Devices' },
+    { id: '2', name: 'Medical Practice' },
+    { id: '3', name: 'Pharmaceuticals' },
+    { id: '4', name: 'Hospitals & Health Care' },
+];
+
+const filterFields = (fields, query) => {
+    if (!query) {
+        return fields;
+    }
+
+    return fields.filter((field) => {
+        const fieldName = field.name.toLowerCase();
+        return fieldName.includes(query);
+    });
+};
+
+const App = () => {
+    const { search } = window.location;
+    const query = new URLSearchParams(search).get('search_field');
+    const [searchQuery, setQuery] = useState(query || '');
+    const filteredFields = filterFields(fields, searchQuery);
+
+    return (
+        <Router>
+            <div className="App">
+                <Announcer
+                    message={`${filteredFields.length} fields`}
+                />
+                <Search
+                    searchQuery={searchQuery}
+                    setQuery={setQuery}
+                />
+                <ul>
+                    {filteredFields.map((field) => (
+                        <li key={field.id}>{field.name}</li>
+                    ))}
+                </ul>
+            </div>
+        </Router>
+    );
+};
+
+export default App;
