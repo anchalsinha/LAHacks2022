8f5cc0828f0d521c47f3f02bdbb53cf153a97783
Anchal Sinha <anchalsinha@ucla.edu>
2022-04-16 21:09:14 -0700

    Add mysql integration

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
 const express = require('express')
+const mysql = require('mysql')
+
 const port = process.env.PORT || 3000;
 
 const app = express()
+app.use(express.json())
 
-// app.use(express.static("src"));
+const db = mysql.createConnection({
+	host: "localhost",
+	user: "cs143",
+	password: "",
+	database: "class_db",
+});
 
+db.connect(function(err) {
+	if (err) throw err;
+	console.log("You're successfully connected to MySQL");
+});
 
+app.get('/latlong', (req, res) => {
+  // request fields
+  // - industry name
+  
+  // return list of lat and list of long
+
+  industry_name = req.body.industry
+
+  db.query(`SELECT name, lat, lon FROM companies WHERE industry = ${industry_name}`, function(err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  })
+
+  res.send({ lat: [], lon: [] });
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
